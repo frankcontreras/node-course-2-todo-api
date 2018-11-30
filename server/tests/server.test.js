2be6343e92a91ac092dca69009b34062a2aa0aa2
Frank Contreras <fcontreras@Franks-MacBook-Pro.local>
2018-11-30 10:40:24 -0600

    Setup app for keroku

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,11 +1,14 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-const todos = [{text: 'First test todo'}, {text:
-'Second test todo'}];
+const todos = [
+  {_id: new ObjectID(), text: 'First test todo'},
+  {_id: new ObjectID(), text: 'Second test todo'}
+];
 
 beforeEach((done) => {
   Todo.remove({}).then(() => {
@@ -61,6 +64,18 @@ describe('Get /todos', () => {
       .expect(200)
       .expect((res) => {
         expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.txt).toBe(todos[0].text);
       })
       .end(done);
   });
